c775f70621efb975768be25e6771cdeb58626c38
Yegor Pelykh <yegor.dev@gmail.com>
2019-09-04 23:17:43 +0300

    Added map widgets, implemented location marker

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 
-class MyApp extends StatelessWidget {
+import 'restricted_data.dart' as RestrictedData;
+
+void main() => runApp(GeoChatHunterApp());
+
+class GeoChatHunterApp extends StatefulWidget {
+  const GeoChatHunterApp({Key key}) : super(key: key);
+
+  @override
+  _GeoChatHunterAppState createState() => _GeoChatHunterAppState();
+}
+
+class _GeoChatHunterAppState extends State<GeoChatHunterApp>
+    with SingleTickerProviderStateMixin {
+  final List<Tab> _tabs = [
+    Tab(text: 'Map', icon: Icon(Icons.map)),
+    Tab(text: 'GeoChats', icon: Icon(Icons.view_list))
+  ];
+  final LatLng _startingCenter = LatLng(51.5, -0.09);
+
+  TabController _tabController;
+  MapController _mapController;
+  LatLng _centerLocation;
+  LatLng _markerLocation;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: _tabs.length);
+    _mapController = MapController();
+    _markerLocation = _startingCenter;
+    _centerLocation = _startingCenter;
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'GeoChat Hunter',
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
+      home: Scaffold(
+        appBar: AppBar(
             title: const Text('GeoChat Hunter'),
-            bottom: TabBar(
-              tabs: [
-                Tab(text: 'Map', icon: Icon(Icons.map)),
-                Tab(text: 'GeoChats', icon: Icon(Icons.view_list))
+            bottom: TabBar(controller: _tabController, tabs: _tabs)),
+        body: TabBarView(
+          controller: _tabController,
+          physics: NeverScrollableScrollPhysics(),
+          children: [
+            FlutterMap(
+              mapController: _mapController,
+              options: MapOptions(
+                  onPositionChanged: (MapPosition position, bool hasGesture) {
+                    _centerLocation = position.center;
+                  },
+                  onTap: (LatLng location) {
+                    setState(() {
+                      _markerLocation = location;
+                    });
+                  },
+                  center: _centerLocation,
+                  zoom: 13.0),
+              layers: [
+                TileLayerOptions(
+                    urlTemplate: RestrictedData.urlTemplate,
+                    additionalOptions: RestrictedData.additionalOptions
+                ),
+                MarkerLayerOptions(markers: [
+                  Marker(
+                      anchorPos: AnchorPos.align(AnchorAlign.top),
+                      width: 50.0,
+                      height: 50.0,
+                      point: _markerLocation,
+                      builder: (ctx) => Container(
+                          child: Icon(Icons.location_on,
+                              color: Colors.red, size: 50)))
+                ]),
               ],
-            )
-          ),
-          body: TabBarView(
-            children: [
-              Icon(Icons.map),
-              Icon(Icons.view_list)
-            ],
-          ),
+            ),
+            Icon(Icons.view_list)
+          ],
         ),
       ),
     );
   }
 }
-
-void main() => runApp(MyApp());
